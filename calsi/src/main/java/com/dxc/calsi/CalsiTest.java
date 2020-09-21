e20ce6c05186aa61d79cfda63df89b874a622bcf
javeedmohammedjaveed@gmail.com <javeedmohammedjaveed@gmail.com>
2020-09-21 10:29:15 -0700

    Calsi class added

---
+++ calsi/src/main/java/com/dxc/calsi/CalsiTest.java
@@ -0,0 +1,26 @@
+package com.dxc.calsi;
+
+public class CalsiTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int result = 	add(4,5);
+		
+			System.out.println(result);
+		
+	}
+	
+	
+	
+	
+	
+	public static int add(int  num1,int num2) {
+		
+		return num1+num2;
+		
+	}
+	
+	
+
+}
